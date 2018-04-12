52b14306d24311ffbae7f0b2d31ce633ac6441d8
Claudio Ferreira <filhocf@gmail.com>
2018-04-12 11:13:30 -0300

    Integrated all adjusts from rwfalcao, Closes #3 #4 #5, adjust the reference to logo in login page and improve install instructions, Closes #7

--- Template/layout/logintop.php
+++ Template/layout/logintop.php
@@ -2,6 +2,6 @@
 
 <div id="login-top">
 	<?php if (isset($themePlusConfig['login-logo-link'])): ?><a href="<?php echo $themePlusConfig['login-logo-link']; ?>" target="_blank"><?php endif; ?>
-		<img src="<?= $this->url->dir(); ?>plugins/Themeplus/Img/<?= $themePlusConfig['login-logo'] ?>" />
+		<img src="<?= $this->url->dir(); ?>plugins/Themeplus/assets/img/<?= $themePlusConfig['login-logo'] ?>" />
 	<?php if (isset($themePlusConfig['login-logo-link'])): ?></a><?php endif; ?>
 </div>
