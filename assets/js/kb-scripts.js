cb5d0affb2ae7131e7809e7d168a1ecef7f4db9a
rwfalcao <falcaorw@gmail.com>
2018-04-19 14:53:15 -0300

    rows da tela de gantt agora estão alinhadas com reus respectivos títulos, closes #8 ao recolher as colunas de tarefas o título da coluna e o simbolo de + agora estão com melhor visualização, closes #10 A barra com a linha do tempo de datas na tela do gantt agora é fixa quando o scroll teoricamente a esconderia, closes #9

--- assets/js/kb-scripts.js
+++ assets/js/kb-scripts.js
@@ -7,3 +7,36 @@
     });
 
 })(jQuery);
+
+
+// $( document ).ready(function() {
+//     menu = $(".menu-inline");
+//     $(function () {
+//         $(window).scroll(function().bind('scroll', function () {
+//             if (menu.offset().top + menu.innerHeight() <= window.scrollY) {
+//                 alert('end reached');
+//             }
+//         });
+//     })
+// });
+
+// menu = $(".menu-inline");
+// $(window).scroll(function(){
+//   if(menu.offset().top + menu.innerHeight() <= window.scrollY) {
+//     alert('hey!');
+//   }else{
+//
+//   }
+// });
+
+$( document ).ready(function() {
+  menu = $(".menu-inline");
+  $(window).scroll(function(){
+    if(menu.offset().top + menu.innerHeight() <= window.scrollY) {
+      $('.ganttview-hzheader').addClass('fixed');
+      console.log('hey');
+    }else{
+      $('.ganttview-hzheader').removeClass('fixed');
+    }
+  });
+});
